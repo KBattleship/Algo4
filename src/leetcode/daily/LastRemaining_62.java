652bcd5a00504c8ef6ad683d728f428bb9553b8b
CheneyIn <xy410257@163.com>
2020-03-30 23:39:17 +0800

    增加约瑟夫环数学算法

--- src/leetcode/daily/LastRemaining_62.java
+++ src/leetcode/daily/LastRemaining_62.java
@@ -26,7 +26,12 @@ public class LastRemaining_62 {
             3. 使用递归进行
      */
 
-    public static int lastRemaining_1(int n, int m) {
+    /*
+        解题思路一
+        1.时间复杂度：O(n)需要求解的n函数值
+        2.空间复杂度：O(n)函数的递归深度为n,需要使用O(n)的栈空间
+     */
+    private static int lastRemaining_1(int n, int m) {
         LinkedList<Integer> linkedList = new LinkedList<>();
         // 将元素全部放到链表中去
         for (int i = 0; i < n; i++) {
@@ -35,11 +40,8 @@ public class LastRemaining_62 {
         // 只要链表长度不为1就一直循环
         while (linkedList.size() != 1) {
             for (int j = 0; j < m; j++) {
-                System.out.println("index-j:" + j);
                 int prePoint = linkedList.pollFirst();
-                System.out.println("first ele:" + prePoint);
                 if (j != m - 1) {
-                    System.out.println("remove ele:" + prePoint);
                     linkedList.add(prePoint);
                 }
             }
@@ -47,13 +49,27 @@ public class LastRemaining_62 {
         return linkedList.pollFirst();
     }
 
+    /*
+        解题思路二：数学解法
+        1.时间复杂度：O(n)需要求解的n函数值
+        2.空间复杂度：O(1)只需要常量个变量
+     */
+    private static int lastRemaining_2(int n,int m){
+        int ans = 0;
+        // 最后一轮剩下2个人，所以从2开始反推
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + m) % i;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // 解题思路1
-         lastRemaining_1(5, 3);
-         lastRemaining_1(10, 17);
+        lastRemaining_1(5, 3);
+        lastRemaining_1(10, 17);
         // 解题思路2
-        //assert lastRemaining_1(5, 3) == 3;
-        //assert lastRemaining_1(10, 17) == 2;
+        lastRemaining_2(5, 3);
+        lastRemaining_2(10, 17);
         // 解题思路3
         //assert lastRemaining_1(5, 3) == 3;
         //assert lastRemaining_1(10, 17) == 2;
