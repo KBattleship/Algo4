c68fd6e4e33e96a34fb929500451723044d9c3ad
CheneyIn <xy410257@163.com>
2020-04-05 23:55:18 +0800

    add bubble sort

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,29 @@
  **
  ***/
 public class Main {
-    public static void main(String args[]){
 
+    public static void main(String[] args) {
+        int[] array={2,6,4,9,1,8,4};
+        int[] res=bubbleSort(array);
+        for(int i:res){
+            System.out.println(i);
+        }
+
+    }
+
+    public static int[] bubbleSort(int[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j + 1] < array[j]) {
+                    int tem = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tem;
+                }
+            }
+        }
+        return array;
     }
 }
